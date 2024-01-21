7550101cfdeecb60ce85e44a891911510f2da583
Muideen27 <ilorimuideen0000@gmail.com>
2024-01-21 10:05:37 +0100

    Creating the app.js and app.css file for page layout

---
+++ client/src/App.js
@@ -0,0 +1,56 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="#"className="">Myblog</a>
+        <nav>
+          <a href="#">Login</a>
+          <a href="#">Register</a>
+        </nav>
+      </header>
+      <div className="post">
+        <div className="image">
+          <img src="https://www.alxappliedai.com/content/images/size/w1000/2023/11/DALL-E-2023-11-22-12.09.42---Create-a-3_2-aspect-ratio--high-resolution-header-image-for-an-educational-project-titled--Creating-a-Deepfake-Video---The-Trailer-.-The-image-should-.png" alt="" />
+        </div>
+      <div className="texts">
+        <h2>Creating a Deepfake Video - The Trailer</h2>
+        <p className="info">
+          <a className="author">Muideen Ilori</a>
+          <time>2024-01-21 09:39</time>
+        </p>
+        <p className='summary'>Explore deepfake tech powered by AI. Craft unique content responsibly using free AI tools</p>
+      </div>
+        </div>
+        <div className="post">
+        <div className="image">
+          <img src="https://www.alxappliedai.com/content/images/size/w1000/2023/11/DALL-E-2023-11-22-12.09.42---Create-a-3_2-aspect-ratio--high-resolution-header-image-for-an-educational-project-titled--Creating-a-Deepfake-Video---The-Trailer-.-The-image-should-.png" alt="" />
+        </div>
+      <div className="texts">
+        <h2>Creating a Deepfake Video - The Trailer</h2>
+        <p className="info">
+          <a className="author">Muideen Ilori</a>
+          <time>2024-01-21 09:39</time>
+        </p>
+        <p className='summary'>Explore deepfake tech powered by AI. Craft unique content responsibly using free AI tools</p>
+      </div>
+        </div>
+        <div className="post">
+        <div className="image">
+          <img src="https://www.alxappliedai.com/content/images/size/w1000/2023/11/DALL-E-2023-11-22-12.09.42---Create-a-3_2-aspect-ratio--high-resolution-header-image-for-an-educational-project-titled--Creating-a-Deepfake-Video---The-Trailer-.-The-image-should-.png" alt="" />
+        </div>
+      <div className="texts">
+        <h2>Creating a Deepfake Video - The Trailer</h2>
+        <p className="info">
+          <a className="author">Muideen Ilori</a>
+          <time>2024-01-21 09:39</time>
+        </p>
+        <p className='summary'>Explore deepfake tech powered by AI. Craft unique content responsibly using free AI tools</p>
+      </div>
+        </div>
+    </main>
+  );
+}
+
+export default App;
